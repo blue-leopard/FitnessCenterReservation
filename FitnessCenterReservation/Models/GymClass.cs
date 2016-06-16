2c1babafcad2506fb5fb5e2f5fd4779927a65908
Andreas Thyrhaug <chessmachine75@gmail.com>
2016-06-16 15:53:58 +0200

    Added class GymClass.

---
+++ FitnessCenterReservation/Models/GymClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FitnessCenterReservation.Models
+{
+    public class GymClass
+    {
+        public string Name { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public TimeSpan Duration { get; set; }
+
+        public DateTime EndTime {
+            get {
+                return StartTime + Duration;
+            }
+        }
+
+        public string Description { get; set; }
+    }
+}
